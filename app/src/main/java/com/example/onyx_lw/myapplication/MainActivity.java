85d51052caff2f8b4d171cb99e8d14c7dab7f9cb
onyx-lw <liuwei@onyx-international.com>
2016-08-30 20:00:32 +0800

    aaaaarrrr

--- app/src/main/java/com/example/onyx_lw/myapplication/MainActivity.java
+++ app/src/main/java/com/example/onyx_lw/myapplication/MainActivity.java
@@ -15,4 +15,7 @@ public class MainActivity extends AppCompatActivity {
     public int  add(int a,int b){
         return a+b;
     }
+    public int  ad6d(int a,int b){
+        return a+b;
+    }
 }
